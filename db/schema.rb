30a32e4ea76073cbab1247e88385fc79437fdffc
Jul Jen <juliajen.prog@gmail.com>
2018-04-09 23:53:19 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_09_213639) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
